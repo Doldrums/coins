e0f547764f8cf7b4c05090ed341986e63a54144c
doldrums <doldrums.317@gmail.com>
2022-08-07 06:12:06 +0200

    api

---
+++ packages/coingecko_api/lib/coingecko_api.dart
@@ -0,0 +1,4 @@
+library coingecko_api;
+
+export 'src/models/models.dart';
+export 'src/coingecko_api_client.dart';
---
+++ packages/coingecko_api/lib/src/models/crypto_currency.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'crypto_currency.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_CryptoCurrency _$$_CryptoCurrencyFromJson(Map<String, dynamic> json) =>
+    _$_CryptoCurrency(
+      id: json['id'] as String,
+      symbol: json['symbol'] as String,
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$$_CryptoCurrencyToJson(_$_CryptoCurrency instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'symbol': instance.symbol,
+      'name': instance.name,
+    };
