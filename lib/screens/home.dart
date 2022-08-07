d7062304dc08529d08a25a8086a16aa6698b3b13
doldrums <doldrums.317@gmail.com>
2022-08-07 02:59:37 +0200

    minor refactoring

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,6 @@
-import 'package:app_crypto/screens/converter.dart';
 import 'package:app_crypto/screens/settings.dart';
 import 'package:app_crypto/screens/widgets/cards_holder.dart';
+import 'package:app_crypto/screens/widgets/items_holder.dart';
 import 'package:app_crypto/utils/utils.dart';
 import 'package:flutter/material.dart';
 
@@ -82,60 +82,7 @@ class Home extends StatelessWidget {
               ),
             ),
           ),
-          SliverFixedExtentList(
-            itemExtent: 60.0,
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                  child: InkWell(
-                    onTap: () => Utils.showBottomRoute(
-                        context, (_) => const Converter()),
-                    child: Row(
-                      children: [
-                        Container(
-                            width: 40,
-                            height: 40,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12),
-                              color: Colors.blueAccent[100],
-                            ),
-                            child: const Icon(Icons.ac_unit_rounded)),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            text: 'Binance coin\n',
-                            style: Theme.of(context).textTheme.headline4,
-                            children: <TextSpan>[
-                              TextSpan(
-                                text: 'BNB',
-                                style: Theme.of(context).textTheme.headline5,
-                              ),
-                            ],
-                          ),
-                        ),
-                        const Spacer(),
-                        RichText(
-                          text: TextSpan(
-                            text: '123234\n',
-                            style: Theme.of(context).textTheme.headline4,
-                            children: <TextSpan>[
-                              TextSpan(
-                                text: '+1.45%',
-                                style: Theme.of(context).textTheme.subtitle1,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
+          const ItemsHolder(),
         ],
       ),
     );
---
+++ lib/screens/widgets/items_holder.dart
@@ -0,0 +1,66 @@
+import 'package:app_crypto/screens/converter.dart';
+import 'package:flutter/material.dart';
+
+import '../../utils/utils.dart';
+
+class ItemsHolder extends StatelessWidget {
+  const ItemsHolder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverFixedExtentList(
+      itemExtent: 60.0,
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: InkWell(
+              onTap: () =>
+                  Utils.showBottomRoute(context, (_) => const Converter()),
+              child: Row(
+                children: [
+                  Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.blueAccent[100],
+                      ),
+                      child: const Icon(Icons.ac_unit_rounded)),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      text: 'Binance coin\n',
+                      style: Theme.of(context).textTheme.headline4,
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'BNB',
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  RichText(
+                    text: TextSpan(
+                      text: '123234\n',
+                      style: Theme.of(context).textTheme.headline4,
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: '+1.45%',
+                          style: Theme.of(context).textTheme.subtitle1,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
