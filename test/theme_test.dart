f2c386afc28b78519e36044432754e837bc9db6c
doldrums <doldrums.317@gmail.com>
2022-08-10 02:54:50 +0200

    theme tests

---
+++ test/theme_test.dart
@@ -0,0 +1,36 @@
+import 'package:app_crypto/features/theme/cubit/theme_cubit.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'helpers/hydrated_bloc.dart';
+
+void main() {
+  initHydratedStorage();
+
+  group('ThemeCubit', () {
+    test('initial state is correct', () {
+      expect(ThemeCubit().state, ThemeCubit.currentTheme);
+    });
+
+    group('toJson/fromJson', () {
+      test('work properly', () {
+        final themeCubit = ThemeCubit();
+        expect(
+          themeCubit.fromJson(themeCubit.toJson(themeCubit.state)!),
+          themeCubit.state,
+        );
+      });
+    });
+
+    group('updateTheme', () {
+      blocTest<ThemeCubit, ThemeMode>(
+        'emits correct ThemeMode for Theme switch from default',
+        build: ThemeCubit.new,
+        act: (cubit) => cubit.updateTheme(ThemeMode.dark),
+        //TODO
+        expect: () => [ThemeMode.light, ThemeMode.dark],
+      );
+    });
+  });
+}
