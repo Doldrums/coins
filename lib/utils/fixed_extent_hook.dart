3ae1f1a556dd1da3666af04f649e68c4da05a84e
doldrums <doldrums.317@gmail.com>
2022-08-11 03:51:48 +0200

    extend utils + refactoring

--- lib/utils/fixed_extent_hook.dart
+++ lib/utils/fixed_extent_hook.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+part of 'utils.dart';
 
 /// Creates [FixedExtentScrollController] that will be disposed automatically.
 ///
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:intl/intl.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+
+part 'fixed_extent_hook.dart';
 
 class Utils {
   const Utils._();
 
   static void showBottomRoute(
-          BuildContext context, Widget Function(BuildContext) route) =>
+    BuildContext context,
+    Widget Function(BuildContext) route,
+  ) =>
       showBarModalBottomSheet(
         context: context,
         backgroundColor: Colors.transparent,
         builder: route,
       );
+
+  static String prettyNumber(num number) {
+    var formatter = NumberFormat('#,##,000');
+    if(number > 1000) {
+      return formatter.format(number);
+    } else {
+      return number.toStringAsFixed(2);
+    }
+  }
 }
