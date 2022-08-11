24ad26ff8364956bca87efa907e25790a1d0bb4f
doldrums <doldrums.317@gmail.com>
2022-08-11 22:50:52 +0200

    linter

---
+++ lib/generated/locale_keys.g.dart
@@ -0,0 +1,33 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class LocaleKeys {
+  static const home_title = 'home.title';
+  static const home_section_title1 = 'home.section_title1';
+  static const home_section_title2 = 'home.section_title2';
+  static const home = 'home';
+  static const settings_title = 'settings.title';
+  static const settings_section_title1 = 'settings.section_title1';
+  static const settings_theme_item1 = 'settings.theme_item1';
+  static const settings_theme_item2 = 'settings.theme_item2';
+  static const settings_theme_item3 = 'settings.theme_item3';
+  static const settings_section_title2 = 'settings.section_title2';
+  static const settings_section_subtitle = 'settings.section_subtitle';
+  static const settings_locale_title = 'settings.locale.title';
+  static const settings_locale_item1 = 'settings.locale.item1';
+  static const settings_locale_item2 = 'settings.locale.item2';
+  static const settings_locale_item3 = 'settings.locale.item3';
+  static const settings_locale_item4 = 'settings.locale.item4';
+  static const settings_locale = 'settings.locale';
+  static const settings_currency_title = 'settings.currency.title';
+  static const settings_currency_subtitle = 'settings.currency.subtitle';
+  static const settings_currency = 'settings.currency';
+  static const settings_about_title = 'settings.about.title';
+  static const settings_about_subtitle = 'settings.about.subtitle';
+  static const settings_about = 'settings.about';
+  static const settings = 'settings';
+  static const converter_title = 'converter.title';
+  static const converter_subtitle = 'converter.subtitle';
+  static const converter_section_title1 = 'converter.section_title1';
+  static const converter_section_title2 = 'converter.section_title2';
+  static const converter = 'converter';
+}
